146b7c7568697c34fbf94b085c8dbda243130697
Theodore Tasker <ttasker@skillstorm.com>
2022-06-29 17:15:23 -0400

    Updated server path

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config(); 
 const cors = require('cors');
-
+ 
 const app = express();
 const PORT = process.env.PORT || 8080; 
 
 app.use(express.json()); 
 app.use(cors());
-app.use('/flights', require('./routes/flight.route'));
+app.use('/flights', require('./server/routes/flight.route'));
 
-app.all('*', (req, res) => {
+app.all('*', (req, res) => { 
     res.status(404).send('No resources were found');
 });
 
