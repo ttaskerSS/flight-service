d482daa3157ce72dde8e4706980f13ed93bb21ce
Theodore Tasker <ttasker@skillstorm.com>
2022-07-06 15:57:37 -0400

    Added responses

--- server/routes/flight.route.js
+++ server/routes/flight.route.js
@@ -3,27 +3,27 @@ const { createFlight, getAllFlights, getFlight, updateFlight, deleteFlight } = r
 
 router.post('/', async (req, res) => {
     await createFlight(req);
-    res.send("flight created");
-}); 
-  
+    res.send('flight created');
+});
+
 router.get('/', async (req, res) => {
     const flights = await getAllFlights();
     res.json(flights);
 });
 
-router.get('/:id', async (req, res) => {
-    const flight = await getFlight(req.params.id);
+router.get('/:flightNumber', async (req, res) => {
+    const flight = await getFlight(req.params.flightNumber); 
     res.json(flight);
 });
 
 router.put('/:id', async (req, res) => {
-    await updateFlight(req); 
+    await updateFlight(req);  
     res.send("flight updated");
-});
+}); 
 
-router.delete('/:id', async (req, res) => {
-    await deleteFlight(req.params.id);
-    res.send("flight deleted");
+router.delete('/:flightNumber', async (req, res) => {
+    await deleteFlight(req.params.flightNumber); 
+    res.send('flight deleted');
 });
 
 module.exports = router;
